a42c3f6becf8cb6961ee18323b03970592059f80
patrickz12 <patrickzhu12@gmail.com>
2016-06-06 15:20:26 -0700

    Starting Position Printer
    Prints starting position every 2 seconds based on button states

--- potentiometer_code.ino
+++ potentiometer_code.ino
@@ -16,6 +16,8 @@ Servo myServo;                      // compass servo
 /*Potentiometer Variables*/
 int startingBlock; // notes starting block position 0 or 1
 int buttonState = 0;
+float startingPosX; // starting x-position (will vary depending on selected starting position) (NEW)
+float startingPosY; // starting y-position (will vary depending on selected starting position) (NEW)
 
 void setup() {
   Serial.begin(9600);
@@ -28,5 +30,82 @@ void loop() {
   int startingBlock = floor(analogRead(PotPin) / 129);
   Serial.print("Button State:"); Serial.print("\t"); Serial.println(buttonState);
   Serial.print("Starting Block:"); Serial.print("\t"); Serial.println(startingBlock);
-  delay(1);
+  start();
+  Serial.print("startingPosX:"); Serial.print("\t"); Serial.println(startingPosX);
+  Serial.print("startingPosY:"); Serial.print("\t"); Serial.println(startingPosY);
+  delay(2000);
 }
+
+/************** Potentiometer/Starting Position Base Code *************/
+void start() {
+  if (buttonState == 1){ 
+    if (startingBlock == 0){ // Position A1
+      startingPosX = -99;
+      startingPosY = 154;
+    }
+    else if (startingBlock == 1){ // Position A2
+      startingPosX = -121;
+      startingPosY = 132;
+    }
+    else if (startingBlock == 2){ // Position A3
+      startingPosX = -99;
+      startingPosY = 110;
+    }
+    else if (startingBlock == 3){ // Position A4
+      startingPosX = -77;
+      startingPosY = 132;
+    }
+    else if (startingBlock == 4){ // Position B1
+      startingPosX = -33;
+      startingPosY = 154;
+    }
+    else if (startingBlock == 5){ // Position B2
+      startingPosX = -55;
+      startingPosY = 132;
+    }
+    else if (startingBlock == 6){ // Position B3
+      startingPosX = -33;
+      startingPosY = 110;
+    }
+    else if (startingBlock == 7){ // Position B4
+      startingPosX = -11;
+      startingPosY = 132;
+    }
+  }
+  else if (buttonState == 0){
+    startingBlock = floor(analogRead(PotPin) / 129);
+    if (startingBlock == 0){ // Position C1
+      startingPosX = -99;
+      startingPosY = -110;
+    }
+    else if (startingBlock == 1){ // Position C2
+      startingPosX = -121;
+      startingPosY = -132;
+    }
+    else if (startingBlock == 2){ // Position C3
+      startingPosX = -99;
+      startingPosY = -154;
+    }
+    else if (startingBlock == 3){ // Position C4
+      startingPosX = -77;
+      startingPosY = -132;
+    }
+    else if (startingBlock == 4){ // Position D1
+      startingPosX = -33;
+      startingPosY = -110;
+    }
+    else if (startingBlock == 5){ // Position D2
+      startingPosX = -55;
+      startingPosY = -132;
+    }
+    else if (startingBlock == 6){ // Position D3
+      startingPosX = -33;
+      startingPosY = -154;
+    }
+    else if (startingBlock == 7){ // Position D4
+      startingPosX = -11;
+      startingPosY = -132;
+    }
+  }
+  }
+
